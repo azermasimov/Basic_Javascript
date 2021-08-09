47c52d651f8fc882507bbcd025542f2a0c0ca84a
Azer Masimov <azer.mesimov@starex.az>
2021-08-09 12:42:45 +0400

    commit

---
+++ lesson/script.js
@@ -0,0 +1,9 @@
+let youLike = confirm("Do you like to Drink");
+console.log(youLike);
+let whichOne = prompt("What kind of drinks do you like?");
+
+let a = youLike + " " + whichOne;
+//alert(youLike + whichOne);
+alert(a);
+console.log(a);
+
